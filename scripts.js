1bd9c01960a832bdcb3ef6e62d7a7a37c882c09e
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-13 09:37:39 +0000

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const ENDPOINT = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const CITIES = [];
+
+fetch(ENDPOINT)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+  return CITIES.filter(place => {
+    const REGEX = new RegExp(wordToMatch, 'gi');
+    return place.city.match(REGEX) || place.state.match(REGEX)
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+  const MATCHARRAY = findMatches(this.value, cities);
+  const HTML = MATCHARRAY.map(place => {
+    const REGEX = new RegExp(this.value, 'gi');
+    const CITYNAME = place.city.replace(REGEX, `<span class="hl">${this.value}</span>`);
+    const STATENAME = place.state.replace(REGEX, `<span class="hl">${this.value}</span>`);
+
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+  }).join('');
+
+  SUGGESTIONS.innerHTML = html;
+}
+
+const SEARCHINPUT = document.querySelector('.search');
+const SUGGESTIONS = document.querySelector('.suggestions');
+
+SEARCHINPUT.addEventListener('change', displayMatches);
+SEARCHINPUT.addEventListener('keyup', displayMatches);
